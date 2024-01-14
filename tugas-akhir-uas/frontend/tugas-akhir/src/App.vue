f9ab4e71baaa723fe41120d95ef86e01d91e0688
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2024-01-14 17:21:52 +0700

    moved navbar into a component

--- tugas-akhir-uas/frontend/tugas-akhir/src/App.vue
+++ tugas-akhir-uas/frontend/tugas-akhir/src/App.vue
@@ -1,29 +1,17 @@
-<script setup>
-import { ref, computed } from 'vue'
-import Home from './pages/Home.vue'
-import Register from './pages/Register.vue'
-import Login from './pages/Login.vue'
+<script>
+import NavBar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
 
-const routes = {
-  '/' : Home,
-  '/register': Register,
-  '/login': Login
+export default {
+  components: {
+    NavBar,
+    Footer
+  },
+  // ...
 }
-
-const currentPath = ref(window.location.hash)
-
-window.addEventListener('hashchange', () => {
-  currentPath.value = window.location.hash
-})
-
-const currentView = computed(() => {
-  return routes[currentPath.value.slice(1) || '/']
-})
 </script>
 
 <template>
-  <a href="#/">Home</a> |
-  <a href="#/register">Register</a> |
-  <a href="#/login">Login</a>
-  <component :is="currentView" />
+  <NavBar></NavBar>
+  <Footer></Footer>
 </template>
