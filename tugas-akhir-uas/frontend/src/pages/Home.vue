407104553b3330b8bd11c89966daefd1cac521ed
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2024-01-15 01:03:09 +0700

    changed directory

---
+++ tugas-akhir-uas/frontend/src/pages/Home.vue
@@ -0,0 +1,4 @@
+<template>
+  <div>INI TAMPILAN HOME SEMENTARA</div>
+</template>
+
---
+++ tugas-akhir-uas/frontend/src/pages/Profile.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>{{ profile }}</div>
+</template>
+
+<script setup>
+const profile = "please log in first"
+</script>
