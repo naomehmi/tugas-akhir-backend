45d2d76f17082a32b6e7e6cf60859d9933ba9c27
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2024-01-14 16:28:20 +0700

    temp login page

--- tugas-akhir-uas/frontend/tugas-akhir/src/pages/Login.vue
+++ tugas-akhir-uas/frontend/tugas-akhir/src/pages/Login.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
-    <div>Username</div>
-    <input type="text" name="username" id="uname" ref="username">
-    <div>Password</div>
-    <input type="text" name="password" id="pw" ref="password">
-    <div>{{ msg }}</div> 
-    <button @click.prevent="login()">login</button>
-  </div>
+    <div>
+      <div>Username</div>
+      <input type="text" name="username" id="uname" ref="username">
+      <div>Password</div>
+      <input type="text" name="password" id="pw" ref="password">
+      <div>{{ msg }}</div>
+      <button @click.prevent="login()">login</button>
+    </div>
 </template>
 
 <script>
@@ -14,14 +14,28 @@ export default {
   data() {
     return {
       uname: '',
-      pass:'',
-      msg:''
+      pass: '',
+      msg: ''
     };
   },
-  methods : {
-    async login(){
+  methods: {
+    async login() {
       this.uname = this.$refs.username.value
       this.pass = this.$refs.password.value
+
+      const response = await fetch('http://localhost:3000/login', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          "name": this.uname,
+          "password": this.pass
+        })
+      })
+
+      const data = await response.json()
+      this.msg = data.message
     }
   }
 }
