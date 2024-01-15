e67a201d419e73e4e6f8cbc6dd4e1b4459c67ef6
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2024-01-15 20:03:40 +0700

    Add files via upload

---
+++ tugas-akhir-uas/frontend/src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>email:<a>CustomerService@FruityBliss.co.id</a>
+    <br>
+    Check out our socials
+    <br>
+    <a><img src="#"></a>
+    <a><img src="#"></a>
+    <a><img src="#"></a>
+  </div>
+</template>
